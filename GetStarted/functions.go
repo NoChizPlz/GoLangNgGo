7fd4e24de5c667e9f667f676c83222b17d126930
Joshua Lictan <joshua.lictan@nccgroup.com>
2024-07-24 21:42:23 +0800

    update functions

--- GetStarted/functions.go
+++ GetStarted/functions.go
@@ -15,6 +15,37 @@ func concatStringInt(str string, num int) (result string) {
 	return
 }
 
+func process(x int) (squared int, cubed int) {
+	squared = square(x)
+	cubed = cube(x)
+	return
+}
+
+func square(number int) (result int) {
+	result = number
+	for i := 0; i < 1; i++ {
+		result *= result
+	}
+	return
+}
+
+func cube(number int) (result int) {
+	result = number
+	for i := 0; i < 2; i++ {
+		result = result * number
+	}
+	return
+}
+
+func factorial_recursion(x float64) (y float64) {
+	if x > 0 {
+		y = x * factorial_recursion(x-1)
+	} else {
+		y = 1
+	}
+	return
+}
+
 func main() {
 	fmt.Println(sum(1, 2))
 
@@ -23,4 +54,12 @@ func main() {
 	nameAge("Jake", 31)
 
 	fmt.Println(concatStringInt("Sample", 1))
+
+	squared, cubed := process(5)
+	fmt.Println("Squared:", squared, "Cubed:", cubed)
+
+	a, _ := process(100)
+	fmt.Println(a)
+
+	fmt.Println(factorial_recursion(4))
 }
